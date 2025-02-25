4669dffaf7e0ed5f59021ae979deba883525ab53
Arinji2 <arinjaydhar205@gmail.com>
2025-02-25 11:49:43 +0530

    db: Add Providers Migrations and Cleanup Formatting

--- db/migrations/20250225040011_users.sql
+++ db/migrations/20250225040011_users.sql
@@ -1,15 +1,26 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE users (
- id INTEGER PRIMARY KEY,
- username VARCHAR(255) NOT NULL,
- email VARCHAR(255) UNIQUE NOT NULL,
- createdAt DATETIME NOT NULL
+  id INTEGER PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  createdAt DATETIME NOT NULL
+);
+
+CREATE TABLE providers (
+  id INTEGER PRIMARY KEY,
+  userId INTEGER NOT NULL,
+  type VARCHAR(255) NOT NULL,
+  refreshToken VARCHAR(255) NOT NULL,
+  accessToken VARCHAR(255) NOT NULL,
+  expiresAt DATETIME NOT NULL,
+  FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
 );
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE users;
+DROP TABLE providers;
 -- +goose StatementEnd
 
 
