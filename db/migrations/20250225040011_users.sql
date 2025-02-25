ef42bb3ffe62d9f47f57cad00d599a47a5ff643b
Arinji2 <arinjaydhar205@gmail.com>
2025-02-25 10:10:45 +0530

    setup: Update DB Directory to use migrations

---
+++ db/migrations/20250225040011_users.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+ id INTEGER PRIMARY KEY,
+ username VARCHAR(255) NOT NULL,
+ email VARCHAR(255) UNIQUE NOT NULL,
+ createdAt DATETIME NOT NULL
+);
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE users;
+-- +goose StatementEnd
+
+
