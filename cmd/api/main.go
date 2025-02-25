001cb62a8ad56222a705280a1c6d27300d49e3fd
Arinji2 <arinjaydhar205@gmail.com>
2025-02-25 11:15:42 +0530

    api: Setup net/http for main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/arinji2/vocab-thing/db"
+	"github.com/arinji2/vocab-thing/routes"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
 	db, err := db.SetupDatabase("../../db/app.db")
 	if err != nil {
 		log.Fatal(err)
@@ -16,4 +17,15 @@ func main() {
 	defer db.Close()
 
 	fmt.Println("Database setup complete and ready to use.")
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: routes.RegisterRoutes(db),
+	}
+
+	log.Println("Starting server on port 8080")
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
