d0b53fa6b4cbb28bfc168ab3ceadddbe919005f0
Arinji2 <arinjaydhar205@gmail.com>
2025-02-25 11:50:02 +0530

    api: Setup Get All Users Handler

--- cmd/api/handlers/user.go
+++ cmd/api/handlers/user.go
@@ -1,10 +1,28 @@
 package handlers
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/arinji2/vocab-thing/internal/models/sqlite"
+)
 
 type UserHandler struct {
 	*Handler
 }
 
 func (h *UserHandler) GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	userModel := sqlite.UserModel{DB: h.DB}
+	users, err := userModel.All(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	for _, user := range users {
+		w.Write([]byte(user.Username))
+	}
 }
