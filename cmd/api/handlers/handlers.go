eb04595559290a1ab8b1951553c9b623a694b7fb
Arinji2 <arinjaydhar205@gmail.com>
2025-03-05 00:19:40 +0530

    api: Remove Unused Helper Function

--- cmd/api/handlers/handlers.go
+++ cmd/api/handlers/handlers.go
@@ -2,10 +2,6 @@ package handlers
 
 import (
 	"database/sql"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"net/http"
 )
 
 type Handler struct {
@@ -15,20 +11,4 @@ type Handler struct {
 // NewHandler creates a new base Handler.
 func NewHandler(db *sql.DB) *Handler {
 	return &Handler{DB: db}
-}
-
-// parseRequestBody parses the JSON body of a request into the provided struct.
-func parseRequestBody(r *http.Request, data interface{}) error {
-	if r.Body == nil {
-		return errors.New("request body is empty")
-	}
-	defer r.Body.Close()
-
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(data)
-	if err != nil {
-		return fmt.Errorf("invalid request body: %w", err)
-	}
-
-	return nil
 }
