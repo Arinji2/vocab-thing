dee68cbc68a45abfd25c662772df1c6fdfabecf4
Arinji2 <arinjaydhar205@gmail.com>
2025-03-05 00:21:19 +0530

    api: Update idgen to not return errors

--- cmd/api/internal/utils/idgen/idgen.go
+++ cmd/api/internal/utils/idgen/idgen.go
@@ -2,17 +2,12 @@ package idgen
 
 import (
 	"crypto/rand"
-	"fmt"
 	"io"
 	"strings"
 )
 
 // GenerateRandomID generates a cryptographically random ID string of the specified length and charset
-func GenerateRandomID(length int, charset string) (string, error) {
-	if length <= 0 {
-		return "", fmt.Errorf("length must be positive")
-	}
-
+func GenerateRandomID(length int, charset string) string {
 	if length == 0 {
 		length = DefaultIDSize
 	}
@@ -25,15 +20,12 @@ func GenerateRandomID(length int, charset string) (string, error) {
 	charSetLen := len(charSetBytes)
 
 	randomBytes := make([]byte, length)
-	_, err := io.ReadFull(rand.Reader, randomBytes)
-	if err != nil {
-		return "", fmt.Errorf("failed to generate random bytes: %w", err)
-	}
+	io.ReadFull(rand.Reader, randomBytes)
 
 	idBuilder := strings.Builder{}
 	for _, b := range randomBytes {
 		idBuilder.WriteByte(charSetBytes[int(b)%charSetLen])
 	}
 
-	return idBuilder.String(), nil
+	return idBuilder.String()
 }
