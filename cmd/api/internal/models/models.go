a348b3ee95b5d9904961cea30fdf4d54279261c6
Arinji2 <arinjaydhar205@gmail.com>
2025-03-07 02:30:28 +0530

    api: Add SQL Mappings to Structs, make models identical to sql order

--- cmd/api/internal/models/models.go
+++ cmd/api/internal/models/models.go
@@ -3,21 +3,20 @@ package models
 import "time"
 
 type User struct {
-	ID        string    `json:"id"`
-	Username  string    `json:"username"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        string    `json:"id" sql:"id"`
+	Username  string    `json:"username" sql:"username"`
+	Email     string    `json:"email" sql:"email"`
+	CreatedAt time.Time `json:"created_at" sql:"createdAt"`
 }
 
 type OauthProvider struct {
-	ID             string    `json:"id"`
-	Type           string    `json:"type"`
-	UserID         string    `json:"user_id"`
-	ProviderUserID string    `json:"provider_user_id"`
-	AccessToken    string    `json:"access_token"`
-	ExpiresIn      time.Time `json:"expires_in"`
-	RefreshToken   string    `json:"refresh_token"`
-	CreatedAt      string    `json:"created_at"`
+	ID           string    `json:"id" sql:"id"`
+	UserID       string    `json:"user_id" sql:"userId"`
+	Type         string    `json:"type" sql:"type"`
+	RefreshToken string    `json:"refresh_token" sql:"refreshToken"`
+	AccessToken  string    `json:"access_token" sql:"accessToken"`
+	ExpiresAt    time.Time `json:"expires_at" sql:"expiresAt"`
+	CreatedAt    time.Time `json:"created_at" sql:"createdAt"`
 }
 
 type Session struct {
