4ca4cdf49eab0736de64646ba7adbb7e13d0aaf9
Arinji2 <arinjaydhar205@gmail.com>
2025-03-06 23:37:48 +0530

    api: Add WriteJson Utility

--- cmd/api/handlers/handlers.go
+++ cmd/api/handlers/handlers.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/json"
+	"net/http"
 )
 
 type Handler struct {
@@ -11,4 +13,10 @@ type Handler struct {
 // NewHandler creates a new base Handler.
 func NewHandler(db *sql.DB) *Handler {
 	return &Handler{DB: db}
+}
+
+func writeJSON(w http.ResponseWriter, statusCode int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
 }
