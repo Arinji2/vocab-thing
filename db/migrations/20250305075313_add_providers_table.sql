62a88775e3b351e0516d6fd5b55d1e41e7f999d8
Arinji2 <arinjaydhar205@gmail.com>
2025-03-07 02:29:13 +0530

    db: Add CreatedAt timestamp, remove extra Primary

--- db/migrations/20250305075313_add_providers_table.sql
+++ db/migrations/20250305075313_add_providers_table.sql
@@ -2,11 +2,12 @@
 -- +goose StatementBegin
 CREATE TABLE providers (
   id TEXT PRIMARY KEY,
-  userId TEXT PRIMARY KEY,
+  userId TEXT NOT NULL,
   type VARCHAR(255) NOT NULL,
   refreshToken VARCHAR(255) NOT NULL,
   accessToken VARCHAR(255) NOT NULL,
   expiresAt DATETIME NOT NULL,
+  createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
 );
 -- +goose StatementEnd
