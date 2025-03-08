e70361e2e7e69b5a57482bce5e4227380a96f50e
Arinji2 <arinjaydhar205@gmail.com>
2025-03-09 02:35:31 +0530

    web: Add Buttons for all providers and guest login

--- cmd/web/src/app/page.client.tsx
+++ cmd/web/src/app/page.client.tsx
@@ -2,7 +2,7 @@
 
 export default function HomeClient() {
   return (
-    <div className="">
+    <div className=" flex flex-col items-center justify-center gap-10">
       <button
         onClick={async () => {
           const response = await fetch(
@@ -23,6 +23,61 @@ export default function HomeClient() {
         }}
       >
         Google Login
+      </button>
+      <button
+        onClick={async () => {
+          const response = await fetch(
+            "http://localhost:8080/oauth/generate-code-url",
+            {
+              method: "POST",
+              credentials: "include",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                providerType: "discord",
+              }),
+            },
+          );
+          const url = await response.text();
+          window.location.href = url;
+        }}
+      >
+        Discord Login
+      </button>
+      <button
+        onClick={async () => {
+          const response = await fetch(
+            "http://localhost:8080/oauth/generate-code-url",
+            {
+              method: "POST",
+              credentials: "include",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                providerType: "github",
+              }),
+            },
+          );
+          const url = await response.text();
+          window.location.href = url;
+        }}
+      >
+        Github Login
+      </button>
+      <button
+        onClick={async () => {
+          await fetch("http://localhost:8080/user/create/guest", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        }}
+      >
+        Guest Login
       </button>
     </div>
   );
