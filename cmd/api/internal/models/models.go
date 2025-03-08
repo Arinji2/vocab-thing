0d5d0a640aa5708d65e2638da8b2fbd8f105ffa7
Arinji2 <arinjaydhar205@gmail.com>
2025-03-08 23:44:42 +0530

    api: Add Nullable ProviderID for Session Model

--- cmd/api/internal/models/models.go
+++ cmd/api/internal/models/models.go
@@ -20,11 +20,12 @@ type OauthProvider struct {
 }
 
 type Session struct {
-	ID          string    `json:"id" sql:"id"`
-	UserID      string    `json:"user_id" sql:"userId"`
-	ProviderID  string    `json:"provider_id" sql:"providerId"`
-	Fingerprint string    `json:"fingerprint" sql:"fingerprint"`
-	IP          string    `json:"ip" sql:"ip"`
-	ExpiresAt   time.Time `json:"expires_at" sql:"expiresAt"`
-	CreatedAt   time.Time `json:"created_at" sql:"createdAt"`
+	ID           string    `json:"id" sql:"id"`
+	UserID       string    `json:"user_id" sql:"userId"`
+	ProviderID   string    `json:"provider_id" sql:"providerId"`
+	ProviderType *string   `json:"provider_type,omitempty" sql:"type"`
+	Fingerprint  string    `json:"fingerprint" sql:"fingerprint"`
+	IP           string    `json:"ip" sql:"ip"`
+	ExpiresAt    time.Time `json:"expires_at" sql:"expiresAt"`
+	CreatedAt    time.Time `json:"created_at" sql:"createdAt"`
 }
