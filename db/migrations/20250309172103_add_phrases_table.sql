4c1409a153774f3bd4e0d93c2caf86ce50e00ee4
Arinji2 <arinjaydhar205@gmail.com>
2025-03-13 18:34:12 +0530

    db: Remove Virtual Search Tables

--- db/migrations/20250309172103_add_phrases_table.sql
+++ db/migrations/20250309172103_add_phrases_table.sql
@@ -22,45 +22,9 @@ CREATE TABLE phrase_tags (
   FOREIGN KEY (phraseId) REFERENCES phrases(id) ON DELETE CASCADE
 );
 
--- Create an FTS5 virtual table for full-text search on phrases
-CREATE VIRTUAL TABLE phrases_fts USING fts5(
-  phrase,
-  content='phrases',
-  content_rowid='id'
-);
-
--- Indexes for better performance
-CREATE INDEX idx_phrases_foundIn ON phrases(foundIn);
-CREATE INDEX idx_phrase_tags_tagName ON phrase_tags(tagName);
-
--- Trigger to keep FTS table in sync
-CREATE TRIGGER phrases_ai AFTER INSERT ON phrases
-BEGIN
-  INSERT INTO phrases_fts(rowid, phrase) VALUES (new.id, new.phrase);
-end
-;
-
-CREATE TRIGGER phrases_ad AFTER DELETE ON phrases
-BEGIN
-  DELETE FROM phrases_fts WHERE rowid = old.id;
-end
-;
-
-CREATE TRIGGER phrases_au AFTER UPDATE ON phrases
-BEGIN
-  UPDATE phrases_fts SET phrase = new.phrase WHERE rowid = old.id;
-end
-;
-
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
-DROP TRIGGER phrases_ai;
-DROP TRIGGER phrases_ad;
-DROP TRIGGER phrases_au;
-DROP INDEX idx_phrases_foundIn;
-DROP INDEX idx_phrase_tags_tagName;
-DROP TABLE phrases_fts;
 DROP TABLE phrase_tags;
 DROP TABLE phrases;
 
