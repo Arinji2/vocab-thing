309bd23014b534e259fdb40155cc33ba201e8869
Arinji2 <arinjaydhar205@gmail.com>
2025-03-19 11:56:30 +0530

    api: Update time util to follow idomatic go

--- cmd/api/internal/utils/time.go
+++ cmd/api/internal/utils/time.go
@@ -1,14 +1,13 @@
 package utils
 
 import (
-	"fmt"
 	"time"
 )
 
-func StringToTime(s string, errorMessage string) (time.Time, error) {
+func StringToTime(s string) (time.Time, error) {
 	t, err := time.Parse(time.RFC3339, s)
 	if err != nil {
-		return time.Now().UTC(), fmt.Errorf("%s: %w", errorMessage, err)
+		return time.Now().UTC(), err
 	}
 	return t, nil
 }
