03dbd2026bb55a1ef1f6538daa459d5a99957666
Arinji2 <arinjaydhar205@gmail.com>
2025-03-24 01:40:40 +0530

    web: Add Brand Colors

--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-}
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          text: "#e5f9fa",
+          background: "#041518",
+          primary: {
+            DEFAULT: "#89dfe9",
+            dark: "#0f2d30",
+          },
+          secondary: {
+            DEFAULT: "#4a1882",
+            dark: "#260e40",
+          },
+          accent: "#df4fd6",
+        },
+      },
+    },
+  },
+};
