094b4136844d136c252fcc32f506b174c1a9f2cd
Arinji2 <arinjaydhar205@gmail.com>
2025-03-24 13:13:08 +0530

    web: Update Tailwind Config and Add Prettier

--- web/tailwind.config.mjs
+++ web/tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      letterSpacing: {
+        small: "0.04rem",
+      },
       colors: {
         brand: {
           text: "#e5f9fa",
