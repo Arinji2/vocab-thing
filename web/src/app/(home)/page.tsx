f52ede45ad3182c311c52054c62b25fe926a6630
Arinji2 <arinjaydhar205@gmail.com>
2025-03-30 18:50:26 +0530

    web: Add and use screen-padding utility

--- web/src/app/(home)/page.tsx
+++ web/src/app/(home)/page.tsx
@@ -6,7 +6,7 @@ import Works from "@/app/(home)/works";
 
 export default function Page() {
   return (
-    <div className="flex h-fit w-full pb-10 gap-20 px-4 md:px-2 2xl:px-0 flex-col items-center justify-start">
+    <div className="flex h-fit w-full pb-10 gap-20 screen-padding flex-col items-center justify-start">
       <Hero />
       <Works />
       <Features />
