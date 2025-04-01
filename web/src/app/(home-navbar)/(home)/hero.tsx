49bb283bd52433da9867cc291f03fa0ea2b78ab7
Arinji2 <arinjaydhar205@gmail.com>
2025-04-01 22:58:42 +0530

    web: Update Imports

--- web/src/app/(home-navbar)/(home)/hero.tsx
+++ web/src/app/(home-navbar)/(home)/hero.tsx
@@ -1,4 +1,3 @@
-import { OtherWords } from "@/app/(home-navbar)/(home)/other-word";
 import { Words, WordsLoading } from "./word";
 import { Button } from "@/components/ui/button";
 import { ErrorWrapper } from "@/components/ui/error-boundary";
