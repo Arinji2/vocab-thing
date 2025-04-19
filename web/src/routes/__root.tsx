0605ff17cff983de57be088f13cd4059ae34a605
Arinji2 <arinjaydhar205@gmail.com>
2025-04-19 22:20:14 +0530

    web: Move metadata into new web

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -6,13 +6,11 @@ import {
 } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
-import Header from '../components/Header'
-
 import TanstackQueryLayout from '../integrations/tanstack-query/layout'
-
 import appCss from '../styles.css?url'
 
 import type { QueryClient } from '@tanstack/react-query'
+import { generateDescription, generateTitle } from '@/lib/metadata'
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -21,15 +19,26 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   head: () => ({
     meta: [
-      {
-        charSet: 'utf-8',
-      },
+      { charSet: 'utf-8' },
       {
         name: 'viewport',
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        name: 'description',
+        content: generateDescription(),
+      },
+      {
+        title: generateTitle(),
+      },
+      {
+        name: 'keywords',
+        content:
+          'vocab, vocabthing, arinji, arinji.com, arinjay dhar, save words, phrases',
+      },
+      {
+        name: 'theme-color',
+        content: '#89DFE9',
       },
     ],
     links: [
@@ -37,16 +46,39 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         rel: 'stylesheet',
         href: appCss,
       },
+      {
+        rel: 'icon',
+        href: '/metadata/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        rel: 'icon',
+        href: '/metadata/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        rel: 'icon',
+        href: '/metadata/favicon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      { rel: 'icon', href: '/metadata/favicon.svg', type: 'image/svg+xml' },
+      { rel: 'shortcut icon', href: '/metadata/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        href: '/metadata/apple-touch-icon.png',
+        sizes: '180x180',
+      },
+      { rel: 'manifest', href: '/metadata/site.webmanifest' },
     ],
   }),
 
   component: () => (
     <RootDocument>
-      <Header />
-
       <Outlet />
       <TanStackRouterDevtools />
-
       <TanstackQueryLayout />
     </RootDocument>
   ),
