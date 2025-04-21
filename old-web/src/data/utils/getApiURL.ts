7019231a08022a51709eebe41d6380bc4df5ee75
Arinji2 <arinjaydhar205@gmail.com>
2025-04-21 19:49:42 +0530

    web: Remove NextJS stuff finally

--- old-web/src/data/utils/getApiURL.ts
+++
@@ -1,9 +0,0 @@
-export function getApiURL() {
-  const envURL = process.env.API_URL;
-  if (envURL) {
-    return envURL;
-  } else {
-    console.error("API_URL is not set");
-    return "http://localhost:8080";
-  }
-}
