f7f5a4145354a29257024d7c43f553bceddc4010
Arinji2 <arinjaydhar205@gmail.com>
2025-04-26 18:41:24 +0530

    web: Create Dashboard Page

--- web/src/routes/dashboard/index.tsx
+++ web/src/routes/dashboard/index.tsx
@@ -1,13 +1,25 @@
+import { RouteError } from '@/components/error'
+import { useSyncing } from '@/lib/is-syncing'
+import { usePhrases } from '@/lib/queries/phrase/index'
+import { useQueryClient } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { useEffect } from 'react'
 
 export const Route = createFileRoute('/dashboard/')({
   component: RouteComponent,
+  errorComponent: () => <RouteError />,
 })
 
 function RouteComponent() {
+  const { isSyncing } = useSyncing()
+  const { isLoading, isError, data, error } = usePhrases(isSyncing)
+  const queryClient = useQueryClient()
+  useEffect(() => {
+    console.log('data', data)
+  }, [data])
   return (
-    <div>
-      <h1>Hello "/dashboard/"!</h1>
+    <div className="w-full h-full-navbar flex flex-col items-center justify-center">
+      {data && data.success && <div className="text-white"></div>}
     </div>
   )
 }
