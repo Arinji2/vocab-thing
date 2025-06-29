f881b7a5c02f069fe8c459e786c187a4284e1693
Arinji2 <arinjaydhar205@gmail.com>
2025-06-29 08:53:49 +0530

    web: Remove syncing

--- web/src/routes/dashboard/index.tsx
+++ web/src/routes/dashboard/index.tsx
@@ -1,9 +1,7 @@
 import { RouteError } from '@/components/error'
-import { useSyncing } from '@/lib/is-syncing'
 import { usePhrases } from '@/lib/queries/phrase/index'
 import { useQueryClient } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import { useEffect } from 'react'
 
 export const Route = createFileRoute('/dashboard/')({
   component: RouteComponent,
@@ -11,12 +9,8 @@ export const Route = createFileRoute('/dashboard/')({
 })
 
 function RouteComponent() {
-  const { isSyncing } = useSyncing()
-  const { isLoading, isError, data, error } = usePhrases(isSyncing)
+  const { isLoading, isError, data, error } = usePhrases()
   const queryClient = useQueryClient()
-  useEffect(() => {
-    console.log('data', data)
-  }, [data])
   return (
     <div className="w-full h-full-navbar flex flex-col items-center justify-center">
       {data && data.success && <div className="text-white"></div>}
